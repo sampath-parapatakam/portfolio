c3b6f119b8211b016c605fe27e0f3c75996fc94d
Sampath Kumar Reddy <43587115+sampath-parapatakam@users.noreply.github.com>
2019-11-12 12:05:32 -0500

    Portfolio-2

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,15 +4,27 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return(
-      <div class="row">
-        <div class="col-lg-8 col-lg-offset-2">
-          <h1>About Page</h1>
-          <p class="about-me-paragraph">Full Stack Web Developer using MongoDB, Express, React, Node.js, Angular, Express.js (MERN and MEAN) with an emphasis on Frontend web development
-            and mobile using React & Angular. Experienced in building CRUD applications, RESTful APIs using Node.js
-            and express, User Auth and oAuth with JWT and Firebase. Common libraries of use: Axios, Redux, Mongoose,
-            Sequalize, Lodash, Express, body-parser, socket.io, react-router, react-move. Common use of UI libraries: Bootstrap 4,
-            React-Bootstrap, Angular Material, Material-UI.</p>
+      <div className="about">
+      <div className="row">
+        <div className="col-lg-8 col-lg-offset-2">
+          <h1 className="about-me">About Page</h1>
+          <ul className="about-me-paragraph">
+            <li>Full Stack Web Developer using MongoDB, Express, React, Node.js, Angular, Express.js (MERN and MEAN) with an emphasis on Frontend web development
+            and mobile using React & Angular.
+            </li> <br></br>
+            <li>
+              Experienced in building CRUD applications, RESTful APIs using Node.js
+            and express, User Auth and oAuth with JWT and Firebase.
+            </li> <br></br>
+            <li>Common libraries of use: Axios, Redux, Mongoose,
+            Sequalize, Lodash, Express, body-parser, socket.io, react-router, react-move.
+            </li> <br></br>
+            <li> Common use of UI libraries: Bootstrap 4,
+            React-Bootstrap, Angular Material, Material-UI.
+            </li>
+          </ul>
         </div>
+      </div>
       </div>
     )
   }
--- src/components/main.js
+++ src/components/main.js
@@ -11,6 +11,7 @@ import Resume from './resume';
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
+    <Route path="/portfolio" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
